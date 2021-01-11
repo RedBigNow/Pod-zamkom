49c93df54ee5a5c082e859e19cb998aaafa4f4ac
Nikita <nikita_sokolov98@mail.ru>
2021-01-11 17:20:25 +0300

    все страницы готовы. для шапки и подвала сделан адаптив. изменена структура файлов сборки.

--- dev/static/js/main.js
+++ dev/static/js/main.js
@@ -51,6 +51,7 @@ if (!Array.from) {
 
 $(document).ready(function () {
 
+/*
 //Мобильное меню
 if (window.matchMedia("(max-width: 770px)").matches) {
     $(function() {
@@ -79,7 +80,50 @@ if (window.matchMedia("(max-width: 770px)").matches) {
       });
   });
 }
+*/
+(function(){
 
+  var burger = document.querySelector('.header-burger');
+  var header = document.querySelector('.header-container');
+  var body = document.body;
+
+  burger.onclick = function() {
+      header.classList.toggle('header-container--open');
+      burger.classList.toggle('header-burger__close');
+      body.classList.toggle('no-scroll');
+  }
+
+  $(document).on("click", ".header-mobile__bottom .callback", function() {
+    header.classList.toggle('header-container--open');
+    burger.classList.toggle('header-burger__close');
+    body.classList.toggle('no-scroll');
+  });
+
+  $(document).on("click", ".header-mobile__bottom .header-toolbar__search", function() {
+    header.classList.toggle('header-container--open');
+    burger.classList.toggle('header-burger__close');
+    body.classList.toggle('no-scroll');
+  });
+
+}());
+
+
+
+
+var menuItemParent = $( '.nav-menu__item-parent' );
+    menuItemParent.on( 'click', function(e) {
+        
+        var mobileMenu = $(e.target).parent().find( '> ul.nav-submenu' );
+        if( mobileMenu.css('display') == 'none' ) {
+            e.preventDefault();
+            mobileMenu.slideDown();
+            e.stopPropagation();
+        } else if( mobileMenu.css('display') == 'block' ) {
+            e.preventDefault();
+            mobileMenu.slideUp();
+            e.stopPropagation();
+        }
+    });
 
 //Слайдер на первом экране главной страницы
 $('.hero-slider').slick({
@@ -89,14 +133,86 @@ $('.hero-slider').slick({
     rows: 1,
     fade: true,
     cssEase: 'linear',
-    dots: true,
-    dotsClass: "hero-dots",
-    //autoplay: true,
-    //autoplaySpeed: 3000,
+    //dots: true,
+    //dotsClass: "hero-dots",
+    autoplay: false,
+    speed: 500,
     nextArrow: document.querySelector('#hero-next'),
     prevArrow: document.querySelector('#hero-prev')
   });
 
+/*
+//Слайдер с услугами на главной странице
+$('.service-slider__list').slick({
+  infinite: false,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  //rows: 1,
+  fade: true,
+  cssEase: 'linear',
+  //dots: true,
+  //dotsClass: "hero-dots",
+  //autoplay: false,
+  //speed: 500,
+  nextArrow: document.querySelector('#hero-next'),
+  prevArrow: document.querySelector('#hero-prev')
+});
+
+*/
+
+
+ //Progress bar для слайдера на главном экране
+ var percentTime;
+ var tick;
+ var time = .1;
+ var progressBarIndex = 0;
+
+ $('.hero-progress__bar .progressBar').each(function(index) {
+     var progress = "<div class='inProgress inProgress" + index + "'></div>";
+     $(this).html(progress);
+ });
+
+ function startProgressbar() {
+     resetProgressbar();
+     percentTime = 0;
+     tick = setInterval(interval, 10);
+ }
+
+ function interval() {
+     if (($('.hero-slider .slick-track div[data-slick-index="' + progressBarIndex + '"]').attr("aria-hidden")) === "true") {
+         progressBarIndex = $('.hero-slider .slick-track div[aria-hidden="false"]').data("slickIndex");
+         startProgressbar();
+     } else {
+         percentTime += 1 / (time + 9);
+         $('.inProgress' + progressBarIndex).css({
+             width: percentTime + "%"
+         });
+         if (percentTime >= 100) {
+             $('.hero-slider').slick('slickNext');
+             progressBarIndex++;
+             if (progressBarIndex > 2) {
+                 progressBarIndex = 0;
+             }
+             startProgressbar();
+         }
+     }
+ }
+
+ function resetProgressbar() {
+     $('.inProgress').css({
+         width: 0 + '%'
+     });
+     clearInterval(tick);
+ }
+ startProgressbar();
+
+
+ $('.hero-progress__bar-item').click(function () {
+   clearInterval(tick);
+   var goToThisIndex = $(this).find("span").data("slickIndex");
+   $('.hero-slider').slick('slickGoTo', goToThisIndex, false);
+   startProgressbar();
+ });
 
 //Слайдер с отзывами на главной странице
 $('.review-slider___list').slick({
@@ -113,7 +229,6 @@ $('.review-slider___list').slick({
     nextArrow: document.querySelector('#reviews-next'),
     prevArrow: document.querySelector('#reviews-prev')
   });
- 
 
 //Слайдер с категориями товаров
 var catLength = $('.index-category__item').length;
@@ -141,20 +256,19 @@ catSlider.slick({
   asNavFor: '.index-category__list'
 });
 
-//Выводим номер текущего слайда
+//Выводим номер текущего слайда в категориях товаров
 $(".index-category__list").on('afterChange', function(event, slick, currentSlide){
   $("#current").text(currentSlide + 1);
 });
 
-//Выводим общее кол-во слайдов
+//Выводим общее кол-во слайдов с категориями товаров
 $("#total").text(catLength)
 
-//Кастомный autoplay для слайдера с категориями
+//Кастомный autoplay для слайдера с категориями товаров
 function autoplayCategorySlider() {
   $( ".index-category .slick-next" ).trigger( "click" );
 }
 setInterval(autoplayCategorySlider, 10000);
-
 
 //Слайдер с постами из блога
 var blogLength = $('.blog-preview__item').length;
@@ -193,8 +307,6 @@ function autoplayBlobSlider() {
 }
 setInterval(autoplayBlobSlider, 10000);
 
-
-
 //Слайдер с товарами + прогресс бар
   function setProgressProduct(index) {
     const calc = ((index + 1) / ($sliderProduct.slick('getSlick').slideCount)) * 100;
@@ -224,7 +336,6 @@ setInterval(autoplayBlobSlider, 10000);
   });  
   
   setProgressProduct(0);
-
 
 //Слайдер с постами из инсты + прогресс бар
 function setProgressInstagram(index) {
@@ -256,60 +367,182 @@ function setProgressInstagram(index) {
   
   setProgressInstagram(0);
 
+//Слайдер с фотографиями на детальной странице товара (Внутренняя и внешняя отделка)
+var backLength = $(' .product-image__item-back').length;
+var backPrevSlider = $('.preview-back__list');
 
-  $('.accordion-item__title').click(function () {
+$('.product-image__list-back').slick({
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  //autoplay: true,
+  //autoplaySpeed: 2000,
+  fade: true,
+  cssEase: 'linear',
+  asNavFor: backPrevSlider
+});
 
-    var faqItemIcon = $(this).find('.accordion-item__icon');
+backPrevSlider.slick({
+  draggable: false,
+  infinite: true,
+  slidesToShow: backLength,
+  slidesToScroll: 1,
+  focusOnSelect: true,
+  vertical: true,
+  verticalSwiping: true,
+  asNavFor: '.product-image__list-back'
+});
 
-    if(faqItemIcon.hasClass('accordion-item__icon_active')) {
-        faqItemIcon.removeClass('accordion-item__icon_active');
+//Кастомный autoplay для слайдера с фотографиями дверей(Внутренняя отделка)
+function autoplayBackSlider() {
+  $( ".product-image__wrapper .slick-next" ).trigger( "click" );
+}
+setInterval(autoplayBackSlider, 10000);
+
+//Слайдер с фотографиями на детальной странице товара
+$('.product-image__list').slick({
+  infinite: true,
+  slidesToShow: 2,
+  slidesToScroll: 1,
+  rows: 1,
+  //autoplay: true,
+  //autoplaySpeed: 3000,
+  nextArrow: document.querySelector('#productImage-next'),
+  prevArrow: document.querySelector('#productImage-prev')
+});
+
+//Открытие модального окна для товаров из каталогов
+$(".buy1click").click(function(){	  
+
+  var product_name = $(this).closest('.product-item').find('.product-item__title h4').text();
+  $('#orderProductName').val(product_name);
+
+  var product_img = $(this).closest('.product-item').find('.product-item__img img').attr('src');
+  $('.order-img').find('#orderProductImage').attr('src', product_img);
+
+});
+
+//Открытие модального окна для товаров из слайдеров
+$(".buy1click-slider").click(function(){
+		        
+  var product_name_slider = $(this).closest('.product-slider__item').find('.product-slider__item-title h4').text();
+  $('#orderProductName').val(product_name_slider);
+
+  var product_img_slider = $(this).closest('.product-slider__item').find('.product-slider__item-img img').attr('src');
+  $('.order-img').find('#orderProductImage').attr('src', product_img_slider);
+
+});
+
+//Прикрутить к ajax обработчику формы "Консультация"
+$("#ConsultSubmit").click(function() {
+  $.fancybox.close($('#consult'));
+  $.fancybox.open($('#success'));
+
+  setTimeout(function() {
+    $.fancybox.close($('#success'));
+  }, 5000);
+
+}); 
+
+//Прикрутить к ajax обработчику формы "Обратная связь"
+$("#actionSubmit").click(function() {
+  $('.action-form__success').addClass('action-form__success--show');
+  setTimeout(function() {
+    $('.action-form__success').removeClass('action-form__success--show');
+  }, 10000);
+}); 
+
+//Прикрутить к ajax обработчику формы отзывов
+$("#reviewSubmit").click(function() {
+  $.fancybox.close($('#reviewPopup'));
+  $.fancybox.open($('#reviewSuccess'));
+
+  setTimeout(function() {
+    $.fancybox.close($('#reviewSuccess'));
+  }, 5000);
+
+}); 
+
+//Прикрутить к ajax обработчику формы заказа
+$("#orderSubmit").click(function() {
+  $.fancybox.close($('#order'));
+  $.fancybox.open($('#orderSuccess'));
+
+  setTimeout(function() {
+    $.fancybox.close($('#orderSuccess'));
+  }, 5000);
+
+});
+
+//Прикрутить к ajax обработчику формы в контактах
+$("#contactsSubmit").click(function() {
+  $('.contacts-form__success').addClass('contacts-form__success-show');
+  setTimeout(function() {
+    $('.contacts-form__success').removeClass('contacts-form__success-show');
+  }, 5000);
+}); 
+
+//Скрытие и раскрытие блока с текстом для SEO
+$('.seo-open').click(function () {
+  $('.seo-wrapper__hidden').slideToggle('slow');
+  $('.seo-open').toggleClass('seo-close');
+});
+
+//Обрезать отзыв и показать кнопку "Показать полностью" если отзыв имеет больше 300 символов
+$('.review-item').each(function() {
+
+var reviewBtn = $(this).find('.review-item__btn');
+var reviewInfo = $(this).find('.review-item__info');
+
+if (reviewInfo.text().length > 300) {
+  reviewInfo.addClass('review-item__info--clipped');
+  reviewBtn.addClass('review-item__btn-show');
+}
+
+reviewBtn.click(function () {
+  reviewInfo.removeClass('review-item__info--clipped');
+  reviewBtn.removeClass('review-item__btn-show');
+});
+
+});
+
+//Аккордион 
+$('.accordion-item__title').click(function () {
+
+  var faqItemIcon = $(this).find('.accordion-item__icon');
+
+  if(faqItemIcon.hasClass('accordion-item__icon--active')) {
+      faqItemIcon.removeClass('accordion-item__icon--active');
+  } else {
+      faqItemIcon.addClass('accordion-item__icon--active');
+  }
+
+  $(this).parent().toggleClass('accordion-item--active');
+
+});
+
+//Кнопка "Наверх"
+(function () {
+  $(document).on('scroll', function () {
+    if ($(document).scrollTop() > 400) {
+      $('.back-top').show();
     } else {
-        faqItemIcon.addClass('accordion-item__icon_active');
+      $('.back-top').hide();
     }
-
-    $(this).parent().toggleClass('accordion-item_active');
   });
-
-
-  $("#ConsultSubmit").click(function() {
-    $.fancybox.close($('#consult'));
-    $.fancybox.open($('#success'));
-
-    setTimeout(function() {
-      $.fancybox.close($('#success'));
-    }, 5000);
-
-  }); 
-  
-  $("#actionSubmit").click(function() {
-    $('.action-form__success').addClass('action-form__success-show');
-    setTimeout(function() {
-      $('.action-form__success').removeClass('action-form__success-show');
-    }, 10000);
-  }); 
-
-  //Кнопка "Наверх"
-  (function () {
-    $(document).on('scroll', function () {
-      if ($(document).scrollTop() > 400) {
-        $('.back-top').show();
-      } else {
-        $('.back-top').hide();
-      }
-    });
-    $('.back-top').on('click', function () {
-      $('html, body').animate({
-        scrollTop: 0
-      }, 'slow');
-      return false;
-    });
-  })();
-
-  $('.seo-open').click(function () {
-    $('.seo-wrapper__hidden').slideToggle('slow');
-    $('.seo-open').toggleClass('seo-close');
+  $('.back-top').on('click', function () {
+    $('html, body').animate({
+      scrollTop: 0
+    }, 'slow');
+    return false;
   });
+})();
 
+//Инициализация библиотеки с анимациями (AOS)
+AOS.init();
+
+//Инициализация библиотеки с анимациями (Enllax)
+$(window).enllax();
 
 });
 
